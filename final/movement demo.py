7856f664c9c389bac5920410d736eb5fa1de10f0
zeek's mac <zekemedley@gmail.com>
2018-12-27 14:43:42 -0800

    new images

--- final/movement demo.py
+++ final/movement demo.py
@@ -1,4 +1,4 @@
-FRAME_RATE = 4
+FRAME_RATE = 2
 WIDTH = 1000
 HEIGHT  = 1000
 
@@ -9,11 +9,10 @@ pygame.init()
 # names of images here
 # they should be inside quotes and comma seperated
 # ex images = ['image1', 'image2']
-images = ['hardest_left.png', 'hardest_left_fin.png','harder_left.png','harder_left_fin.png','left.png','left_fin.png',
-            'center_left.png','center.png','center_right.png','right.png','right_fin.png','hard_right.png','hard_right_fin.png',
-                'hardest_right.png','hardest_right_fin.png']
-images = ['movement_images/v2/'+im for im in images]
-images = [resize_image(im,(250,250)) for im in images]
+images = ['hardest_left_fin','hardest_left','hard_left_fin','hard_left','center_left','center','center_right','hard_right_right','hard_right',
+            'hard_right_left','hard_right','hardest_right_fin','hardest_right']
+images = ['movement_images/v3/'+im+'.png' for im in images]
+# images = [resize_image(im,(250,250)) for im in images]
 images = [pygame.image.load(im) for im in images]
 
 def image_getter():
@@ -35,11 +34,6 @@ getter = image_getter()
 while go:
     display.fill(bg_color)
     display.blit(next(getter), (500-250/2, 500-250/2))
-    display.blit(next(getter), (500-250/4, 500-250/4))
-    display.blit(next(getter), (500-250/6, 500-250/6))
-    display.blit(next(getter), (500+250/2, 500+250/2))
-    display.blit(next(getter), (500+250/4, 500+250/4))
-    display.blit(next(getter), (500+250/6, 500+250/6))
     for event in pygame.event.get():
         if event.type is pygame.QUIT:
             go = False
